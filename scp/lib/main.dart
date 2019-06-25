a0898d0d6ec475974cff5bffb693a17e020a3c30
Thesmader <dsmarak17@gmail.com>
2019-06-25 20:38:56 +1100

    Basic Appointment Card Layout.
    Signed-off-by: Thesmader <dsmarak17@gmail.com>

--- scp/lib/main.dart
+++ scp/lib/main.dart
@@ -27,35 +27,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  cardBuilder(heightFactor) => SizedBox(
-        height: heightFactor * 0.5,
-        child: Card(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.0)),
-          elevation: 8.0,
-          margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(24.0),
-            child: Container(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Container(
-                height: heightFactor * 0.3,
-                child: Center(
-                  child: ListTile(
-
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-                    title: Container(
-                      height: heightFactor*0.1,
-                      color: Colors.white,
-                      child: Center(child: Text('APPOINTMENTS',textAlign: TextAlign.left,)),
-                    ),
-                    trailing: Container(
-                      padding: EdgeInsets.only(right: 12.0),
-                      child: Icon(Icons.apps, color: Colors.white,size: 50.0,),
+  cardBuilder(heightFactor,textScaleFactor) => SizedBox(
+        height: heightFactor * 0.58,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 12.0),
+          child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0)),
+            elevation: 8.0,
+            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
+            child: Stack(
+              fit: StackFit.passthrough,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(24.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.blue),
+                    child: Container(
+                      height: heightFactor * 0.3,
+                      child: Center(
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0),
+                          title: ShaderMask(
+                            shaderCallback: (rect) {
+                              return LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [Colors.black, Colors.transparent],
+                              ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+                            },
+                              blendMode: BlendMode.dstIn,
+                            child: Container(
+                              height: heightFactor * 0.15,
+                              color: Colors.white,
+                              padding: EdgeInsets.only(left: 20.0),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'APPOINTMENT',
+                                  style: TextStyle(fontSize: 23.0*textScaleFactor,color: Colors.blueAccent,fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+
+                SizedBox(child: Image.asset('assets/scp_app.png',alignment: Alignment.centerRight,),
+                ),],
             ),
           ),
         ),
@@ -63,21 +87,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     var queryWidth = MediaQuery.of(context).size.width;
+    var textScaleFactor = MediaQuery.of(context).textScaleFactor;
     return Scaffold(
       backgroundColor: Colors.white,
       body: ListView(
         scrollDirection: Axis.vertical,
-        physics: BouncingScrollPhysics(),
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 24.0),
             child: Text(
               'SCP',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 40.0,fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
             ),
           ),
-          cardBuilder(queryWidth),
+          cardBuilder(queryWidth,textScaleFactor),
+          cardBuilder(queryWidth,textScaleFactor),
+          cardBuilder(queryWidth,textScaleFactor),
+          cardBuilder(queryWidth,textScaleFactor),
         ],
       ),
     );
