1e4fb8e275664dae2dfd9ac4334ae3108f1acc6a
Thesmader <dsmarak17@gmail.com>
2019-07-02 01:57:57 +1100

    Homepage cards UI implemented.
    Signed-off-by: Thesmader <dsmarak17@gmail.com>

--- scp/lib/main.dart
+++ scp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:scp/cards.dart';
+import 'package:scp/gradients.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,65 +29,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  cardBuilder(heightFactor,textScaleFactor) => SizedBox(
-        height: heightFactor * 0.58,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 12.0),
-          child: Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24.0)),
-            elevation: 8.0,
-            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
-            child: Stack(
-              fit: StackFit.passthrough,
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(24.0),
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.blue),
-                    child: Container(
-                      height: heightFactor * 0.3,
-                      child: Center(
-                        child: ListTile(
-                          contentPadding: EdgeInsets.symmetric(
-                              vertical: 10.0),
-                          title: ShaderMask(
-                            shaderCallback: (rect) {
-                              return LinearGradient(
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
-                                colors: [Colors.black, Colors.transparent],
-                              ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
-                            },
-                              blendMode: BlendMode.dstIn,
-                            child: Container(
-                              height: heightFactor * 0.15,
-                              color: Colors.white,
-                              padding: EdgeInsets.only(left: 20.0),
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  'APPOINTMENT',
-                                  style: TextStyle(fontSize: 23.0*textScaleFactor,color: Colors.blueAccent,fontWeight: FontWeight.bold),
-                                  textAlign: TextAlign.left,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                SizedBox(child: Image.asset('assets/scp_app.png',alignment: Alignment.centerRight,),
-                ),],
-            ),
-          ),
-        ),
-      );
   @override
   Widget build(BuildContext context) {
+    Gradients().init(context);
     var queryWidth = MediaQuery.of(context).size.width;
     var textScaleFactor = MediaQuery.of(context).textScaleFactor;
     return Scaffold(
@@ -98,13 +44,12 @@ class _HomePageState extends State<HomePage> {
             child: Text(
               'SCP',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w500, fontFamily: 'PfDin',letterSpacing: 2),
             ),
           ),
-          cardBuilder(queryWidth,textScaleFactor),
-          cardBuilder(queryWidth,textScaleFactor),
-          cardBuilder(queryWidth,textScaleFactor),
-          cardBuilder(queryWidth,textScaleFactor),
+          appointmentCard(context, queryWidth, textScaleFactor),
+          faqCard(context, queryWidth, textScaleFactor),
+          mentorsCard(context, queryWidth, textScaleFactor),
         ],
       ),
     );
