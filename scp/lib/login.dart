3a5ceffed5edc6fdc6d29d1b40a1b258841b9f1e
Chinmay Kabi <chinmay.kabi@live.com>
2019-07-19 23:05:03 +0530

    Non whitelist login fixed

--- scp/lib/login.dart
+++ scp/lib/login.dart
@@ -124,37 +124,3 @@ class _LoginState extends State<Login> {
     );
   }
 }
-
-// String phoneNumber;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       body: Center(
-//         child: Container(
-//             padding: EdgeInsets.all(25.0),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: <Widget>[
-//                 TextField(
-//                   decoration: InputDecoration(hintText: 'Enter Phone number'),
-//                   onChanged: (value) {
-//                     this.phoneNumber = value;
-//                   },
-//                   controller: phoneController,
-//                 ),
-//                 SizedBox(height: 10.0),
-//                 RaisedButton(
-//                     onPressed: () {
-//                       ScpAuth(context).verifyPhone(phoneController.text);
-//                     },
-//                     child: Text('Verify'),
-//                     textColor: Colors.white,
-//                     elevation: 7.0,
-//                     color: Colors.blue),
-//               ],
-//             )),
-//       ),
-//     );
-//   }
-// }
