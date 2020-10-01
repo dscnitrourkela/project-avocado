1186d70416f6962948a55e0e8a8fd6f9e9dd1eee
Rajnish Singh <rajnish.kumar2998@gmail.com>
2020-10-01 22:46:02 +0530

    added network connectivity status #70 (#107)
    * added network connectivity status
    
    * Update connectivity_handler_widget.dart
    
    A condition was missing for handling the case when the user just opens the app and is already having an internet connection. so added an 'if' condition
    
    Former-commit-id: 664710fa0642aeecf67a0f497f67257889eb8713

--- scp/lib/main.dart
+++ scp/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:scp/booking.dart';
+import 'package:scp/connectivity/connectivity_handler_widget.dart';
 import 'package:scp/firebase/firebaseDBHandler.dart';
 import 'package:scp/timetablecardsplit.dart';
 import 'package:scp/ui/cards.dart';
@@ -86,20 +87,24 @@ class _MyAppState extends State<MyApp> {
   bool _loggedin;
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: checkLogin(),
-        builder: (context, response) {
-          if (response.connectionState == ConnectionState.done) {
-            if (_loggedin) {
-              return HomePage(
-                title: "SCP",
-              ); //Navigator.pushNamed(context, '/homePage');
-            } else {
-              return Login(); //Navigator.pushNamed(context, '/login');
-            }
-          } else
-            return CircularProgressIndicator();
-        });
+    
+    // Added connectivity handler Widget to display snackbars for connection status as required
+    return ConnectivityHandlerWidget(
+      child: FutureBuilder(
+          future: checkLogin(),
+          builder: (context, response) {
+            if (response.connectionState == ConnectionState.done) {
+              if (_loggedin) {
+                return HomePage(
+                  title: "SCP",
+                ); //Navigator.pushNamed(context, '/homePage');
+              } else {
+                return Login(); //Navigator.pushNamed(context, '/login');
+              }
+            } else
+              return CircularProgressIndicator();
+          }),
+    );
   }
 
   Future checkLogin() async {
