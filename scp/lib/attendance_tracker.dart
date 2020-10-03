01c61853f91126f8b4e2c0a80821e32cfd072db2
Astha Nayak <58568514+nayakastha@users.noreply.github.com>
2020-10-03 22:19:41 +0530

    Weekend days managed

--- scp/lib/attendance_tracker.dart
+++ scp/lib/attendance_tracker.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:scp/time_table_resources.dart';
 import 'package:scp/time_table.dart';
-
 import 'package:table_calendar/table_calendar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
 
 class AttendanceTracker extends StatefulWidget {
   @override
@@ -22,31 +22,18 @@ class _AttendanceTrackerState extends State<AttendanceTracker> {
   String markedValue;
   List<String> subjects;
   SharedPreferences pref;
+  DateTime currentDay;
   //String theory = theorySection;
   // String practical = practicalSection;
   static String sequence = sectionSequence;
-
   Map<dynamic, dynamic> codes = TimeTableResources.sequence[sequence];
-
-  List<String> items(BuildContext context, DateTime day) {
-    List<String> subList = new List<String>();
-    List<String> dayList = new List<String>();
-    dayList = codes[day.weekday.toString()];
-    for (var ele in dayList) {
-      if (TimeTableResources.theory[theorySection].containsValue(ele))
-        subList.add(TimeTableResources.theory[theorySection][ele]);
-      else
-        subList.add(TimeTableResources.practical[practicalSection][ele]);
-    }
-    return subList;
-  }
-
   @override
   void initState() {
+    currentDay = DateTime.now();
     _calendarController = CalendarController();
     _events = {};
     _selectedEvents = [];
-    markedValue = "abc";
+
     initPrefs();
     super.initState();
   }
@@ -77,6 +64,32 @@ class _AttendanceTrackerState extends State<AttendanceTracker> {
 
   @override
   Widget build(BuildContext context) {
+    List<String> items(DateTime day) {
+      List<String> subList = new List<String>();
+      List<String> dayList = new List<String>();
+      String weekday = DateFormat('EEEE').format(day);
+      print(weekday);
+      dayList = codes[weekday];
+      print(dayList);
+      for (var ele in dayList) {
+        if (day.weekday <= 5) {
+          if (TimeTableResources.theory[theorySection].containsKey(ele)) {
+            print(TimeTableResources.theory[theorySection][ele].toString());
+            subList
+                .add(TimeTableResources.theory[theorySection][ele].toString());
+          } else if (TimeTableResources.practical[practicalSection]
+              .containsKey(ele)) {
+            print(
+                TimeTableResources.practical[practicalSection][ele].toString());
+            subList.add(
+                TimeTableResources.practical[practicalSection][ele].toString());
+          }
+        }
+      }
+      print(subList);
+      return subList;
+    }
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -98,10 +111,12 @@ class _AttendanceTrackerState extends State<AttendanceTracker> {
             children: <Widget>[
               TableCalendar(
                 events: _events,
+                initialSelectedDay: DateTime.now(),
                 calendarController: _calendarController,
                 initialCalendarFormat: CalendarFormat.month,
                 onDaySelected: (day, events) {
                   setState(() {
+                    currentDay = day;
                     _selectedEvents = events;
                   });
                 },
@@ -144,76 +159,81 @@ class _AttendanceTrackerState extends State<AttendanceTracker> {
               SizedBox(
                 height: 10,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  DropdownButton<String>(
-                      icon: Icon(Icons.keyboard_arrow_down),
-                      iconSize: 16,
-                      elevation: 10,
-                      style: TextStyle(
-                        color: primaryColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
+              (currentDay.weekday <= 5)
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        DropdownButton<String>(
+                            icon: Icon(Icons.keyboard_arrow_down),
+                            iconSize: 16,
+                            elevation: 10,
+                            style: TextStyle(
+                              color: primaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                markedValue = value;
+                              });
+                            },
+                            value: markedValue,
+                            items: items(currentDay).map((entry) {
+                              print("true block");
+                              return DropdownMenuItem(
+                                  value: entry,
+                                  child: Text(
+                                    entry,
+                                    style: TextStyle(color: primaryColor),
+                                  ));
+                            }).toList()),
+                        RaisedButton(
+                            elevation: 10,
+                            color: primaryColor,
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                            child: Text("Enter"),
+                            onPressed: () {
+                              setState(() {
+                                if (_selectedEvents.contains(markedValue)) {
+                                  return;
+                                } else {
+                                  if (_events[
+                                          _calendarController.selectedDay] !=
+                                      null) {
+                                    _selectedEvents.add(markedValue);
+                                    _events[_calendarController.selectedDay]
+                                        .add(markedValue);
+                                  } else {
+                                    _selectedEvents.add(markedValue);
+                                    _events[_calendarController.selectedDay] = [
+                                      markedValue
+                                    ];
+                                  }
+                                }
+                                pref.setString(
+                                    'events', json.encode(encodeMap(_events)));
+                              });
+                            })
+                      ],
+                    )
+                  : Container(),
+              (currentDay.weekday <= 5)
+                  ? Container(
+                      padding: EdgeInsets.only(top: 24.0, bottom: 16.0),
+                      child: Text(
+                        "Here are the classes you missed ",
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontSize: 24,
+                            color: primaryColor),
                       ),
-                      onChanged: (value) {
-                        setState(() {
-                          markedValue = value;
-                        });
-                      },
-                      value: markedValue,
-                      items: items(context, _calendarController.selectedDay)
-                          .map((entry) {
-                        return DropdownMenuItem(
-                            value: entry,
-                            child: Text(
-                              entry,
-                              style: TextStyle(color: primaryColor),
-                            ));
-                      }).toList()),
-                  RaisedButton(
-                      elevation: 10,
-                      color: primaryColor,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
-                      child: Text("Enter"),
-                      onPressed: () {
-                        setState(() {
-                          if (_selectedEvents.contains(markedValue)) {
-                            return;
-                          } else {
-                            if (_events[_calendarController.selectedDay] !=
-                                null) {
-                              _selectedEvents.add(markedValue);
-                              _events[_calendarController.selectedDay]
-                                  .add(markedValue);
-                            } else {
-                              _selectedEvents.add(markedValue);
-                              _events[_calendarController.selectedDay] = [
-                                markedValue
-                              ];
-                            }
-                          }
-                          pref.setString(
-                              'events', json.encode(encodeMap(_events)));
-                        });
-                      })
-                ],
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 24.0, bottom: 16.0),
-                child: Text(
-                  "Here are the classes you missed ",
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w900,
-                      fontSize: 24,
-                      color: primaryColor),
-                ),
-              ),
-              (_selectedEvents.length != null)
+                    )
+                  : Container(),
+              (_selectedEvents.length != null && currentDay.weekday <= 5)
                   ? ListView.builder(
                       scrollDirection: Axis.vertical,
                       shrinkWrap: true,
