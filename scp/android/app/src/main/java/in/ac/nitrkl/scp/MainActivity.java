37c2930d8e197f4170f20c8033482dfe63fe1cad
SriramPatibanda <56649197+SriramPatibanda@users.noreply.github.com>
2020-10-11 20:12:10 +0530

    Upgrading android embedding (#118)
    * add flutter-plugin-dependencies
    
    * migrated the project to make it compatible to new android embedding
    
    * Delete .flutter-plugins-dependencies
    
    * Update MainActivity.kt
    
    * Update MainActivity.kt
    
    * Update MainActivity.java
    
    * Update Application.java

--- scp/android/app/src/main/java/in/ac/nitrkl/scp/MainActivity.java
+++ scp/android/app/src/main/java/in/ac/nitrkl/scp/MainActivity.java
@@ -3,26 +3,26 @@ package in.ac.nitrkl.scp;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugin.common.MethodCall;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    private String CHANNEL = "FAQ_ACTIVITY";
+    private static final String CHANNEL = "FAQ_ACTIVITY";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-        new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-                new MethodChannel.MethodCallHandler() {
-                    @Override
-                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
                         if (call.method.equalsIgnoreCase("startFaqActivity")) {
                             Toast.makeText(getApplicationContext(), "Working", Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
+                }
+        );
     }
 }
+
