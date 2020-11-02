ca5e95161428147611903523b470fb795676f97d
Abel Mathew <notify.abelmathew@gmail.com>
2020-11-03 00:34:18 +0530

    Update for Store Release (#126)
    Former-commit-id: ab5f33350c5021709d9e120fd9d82ba8e3be87c9

--- scp/lib/drawer_screens/dev_info.dart
+++ scp/lib/drawer_screens/dev_info.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 final String aboutText =
     "Developer Student Clubs (DSC) is a Google Developers powered program for university students to learn mobile and web development skills. The clubs will be open to any student interested to learn, ranging from novice developers who are just starting, to advanced developers who want to further their skills. The clubs are intended as a space for students to try out new ideas and collaborate to solve mobile and web development problems.\n\n"
-    "DSC NIT Rourkela (DSC NITR) is a student chapter of DSC India with a motive to nurture developers within the community and solve real-life problems in the community through technology.";
+    "DSC NIT Rourkela is a student chapter of DSC India with a motive to nurture developers within the community and solve real-life problems in the community through technology.";
 
 class DevInfo extends StatelessWidget {
   @override
@@ -73,16 +73,50 @@ class DevInfo extends StatelessWidget {
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                contactCard(context, "Abel Mathew | DSC NITR Lead",
-                    "https://github.com/DesignrKnight"),
+                contactCard(
+                    context, "Abel Mathew", "https://github.com/DesignrKnight"),
+                contactCard(
+                    context, "Abhibhaw Asthana", "https://github.com/abhibhaw"),
                 contactCard(
                     context, "Ankesh Anku", "https://github.com/ankank30"),
                 contactCard(
+                    context, "Astha Nayak", "https://github.com/nayakastha"),
+                contactCard(
                     context, "Chinmay Kabi", "https://github.com/Chinmay-KB"),
                 contactCard(
+                    context, "Reuben Abraham", "http://reubenabraham.com/"),
+                contactCard(
                     context, "Smarak Das", "https://github.com/Thesmader"),
-                contactCard(context, "Reuben Abraham | Designer",
-                    "http://reubenabraham.com/"),
+                contactCard(context, "Patibanda Sriram ",
+                    "https://github.com/SriramPatibanda"),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              child: Center(
+                child: Text(
+                  "CONTRIBUTORS",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'PfDin',
+                    color: Color.fromRGBO(74, 232, 190, 1),
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                contactCard(
+                    context, "Harsh Patel", "https://github.com/hkp27299"),
+                contactCard(context, "Pratik Priyadarsan",
+                    "https://github.com/pratik0197"),
+                contactCard(context, "priyanshu0405",
+                    "https://github.com/priyanshu0405"),
+                contactCard(
+                    context, "Rajnish Singh", "https://github.com/rajnis09"),
               ],
             )
           ],
@@ -93,9 +127,7 @@ class DevInfo extends StatelessWidget {
 
   Widget contactCard(BuildContext context, String name, String contact) {
     //var queryWidth = MediaQuery.of(context).size.width;
-    var textScaleFactor = MediaQuery
-        .of(context)
-        .textScaleFactor;
+    var textScaleFactor = MediaQuery.of(context).textScaleFactor;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: InkWell(
