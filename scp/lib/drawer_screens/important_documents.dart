5732afe5b360ddd5209fa40dc03efd3702bee356
abhibhaw <asthana3110@gmail.com>
2020-12-01 22:53:35 +0530

    Made Important Documents Dynamic
    Fixes #76

--- scp/lib/drawer_screens/important_documents.dart
+++ scp/lib/drawer_screens/important_documents.dart
@@ -1,86 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:scp/utils/sizeConfig.dart';
 
-class ImpDocs extends StatelessWidget {
+class ImpDocs extends StatefulWidget {
+  @override
+  _ImpDocsState createState() => _ImpDocsState();
+}
+
+List<Widget> doc = [];
+
+class _ImpDocsState extends State<ImpDocs> {
+  final fireDocs = Firestore.instance;
+
   @override
   Widget build(BuildContext context) {
+    doc = [];
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Color.fromRGBO(25, 39, 45, 1),
-          title: Text(
-            "Important Documents",
-            style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'PfDin',
-                fontWeight: FontWeight.w600),
-          )
+        backgroundColor: Color.fromRGBO(25, 39, 45, 1),
+        title: Text(
+          "Important Documents",
+          style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'PfDin',
+              fontWeight: FontWeight.w600),
+        ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            docCard(context, "Academic Calendar", "https://nitrkl.ac.in/Academics/AcademicProcess/AcademicCalendar.aspx"),
-            docCard(context, "Academic Forms", "https://nitrkl.ac.in/Academics/AcademicProcess/AcademicForms.aspx"),
-            docCard(context, "Academic Regulations", "https://nitrkl.ac.in/Academics/AcademicProcess/AcademicRegulations.aspx"),
-            docCard(context, "Branch-wise Syllabus", "https://nitrkl.ac.in/Academics/AcademicProcess/Curricula-Syllabi/aspx"),
-            docCard(context, "Communication Handbook", "https://nitrkl.ac.in/docs/Misc/CommunicationHandBook.pdf"),
-            docCard(context, "Curriculum", "https://nitrkl.ac.in/Academics/AcademicProcess/Curricula.aspx"),
-            docCard(context, "Faculty Directory", "https://nitrkl.ac.in/FacultyStaff/EmployeeDirectory/Faculty.aspx"),
-            docCard(context, "Timetable", "https://nitrkl.ac.in/Academics/AcademicProcess/AcademicTimeTable.aspx"),
-            docCard(context, "Counsellor Slot Booking for Online Session", "https://bit.ly/slot_booking"),
-          ],
-        ),
-      )
+          child: FutureBuilder(
+              future: fetchData(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.done:
+                    return SingleChildScrollView(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: doc,
+                    ));
+                  default:
+                    return Container(
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                }
+              })),
     );
   }
 
-  Widget docCard(BuildContext context,String name,String contact){
-    //var queryWidth = MediaQuery.of(context).size.width;
-    var textScaleFactor = MediaQuery.of(context).textScaleFactor;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: (){
-          if(contact!=""){
-            _launchURL(contact);
-          }
-          else{}
-        },
-        child: Card(
-          elevation: 5.0,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Text(name,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: textScaleFactor*20,
-                        fontFamily: 'PfDin',
-                        fontWeight: FontWeight.w800
-                    ),),
-                ),
+  var textScaleFactor = SizeConfig.drawerItemTextSize * 0.05;
 
-                Center(
-                    child: Text(contact,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      fontSize: textScaleFactor*15,
-                      color: Colors.blue,
-                      fontFamily: 'PfDin',
-                      fontWeight: FontWeight.w500
-                  ),)),
-              ],
+  Future fetchData() async {
+    QuerySnapshot snapshot = await fireDocs
+        .collection("impdocs")
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((f) => doc.add(new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: () {
+                if (f.data['link'] != null) _launchURL(f.data['link']);
+              },
+              child: Card(
+                elevation: 5.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          f.data['title'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: textScaleFactor * 20,
+                              fontFamily: 'PfDin',
+                              fontWeight: FontWeight.w800),
+                        ),
+                      ),
+                      Center(
+                        child: Text(
+                          f.data['link'] == null ? "" : f.data['link'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: textScaleFactor * 15,
+                            fontFamily: 'PfDin',
+                            fontWeight: FontWeight.w500,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          )));
+      return snapshot;
+    });
+    snapshot.documents.forEach((f) => print(snapshot.documents.length));
+    return snapshot;
   }
 
   _launchURL(String url) async {
@@ -90,6 +113,4 @@ class ImpDocs extends StatelessWidget {
       throw 'Could not launch $url';
     }
   }
-
-
 }
