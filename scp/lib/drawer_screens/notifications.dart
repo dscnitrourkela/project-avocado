a6e6ad46a3fadf037faf0eccd5418f0a53cfea3c
Abel Mathew <notify.abelmathew@gmail.com>
2020-12-03 09:48:24 +0530

    Chat support for freshers (#147)
    * Appointment Screen Freeze fix
    
    * Live chat enabled
    
    Co-authored-by: Chinmay Kabi <chinmaykabi@gmail.com>

--- scp/lib/drawer_screens/notifications.dart
+++ scp/lib/drawer_screens/notifications.dart
@@ -3,16 +3,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:scp/utils/sizeConfig.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Nots extends StatefulWidget{
+class Nots extends StatefulWidget {
   @override
   _Nots createState() => _Nots();
 }
+
 List<Widget> v = [];
+
 class _Nots extends State<Nots> {
   final databaseReference = Firestore.instance;
   @override
   Widget build(BuildContext context) {
-    v=[];
+    v = [];
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Color.fromRGBO(25, 39, 45, 1),
@@ -22,97 +24,92 @@ class _Nots extends State<Nots> {
                   color: Colors.white,
                   fontFamily: 'PfDin',
                   fontWeight: FontWeight.w600),
-            )
-        ),
-        body: SingleChildScrollView(
-          child: FutureBuilder(
+            )),
+        body: FutureBuilder(
             future: getData(),
-            builder: (context,snapshot){
-              switch(snapshot.connectionState){
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
                 case ConnectionState.done:
                   return SingleChildScrollView(
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: v
-                      )
-                  );
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: v));
                 default:
-                  return Container(
-                    child:Center(
-                      child: CircularProgressIndicator(),
-                    ),
+                  return Center(
+                    child: CircularProgressIndicator(),
                   );
               }
-            }
-          )
-        )
-    );
+            }));
   }
-  var textScaleFactor = SizeConfig.drawerItemTextSize*0.05;
+
+  var textScaleFactor = SizeConfig.drawerItemTextSize * 0.05;
 
   Future getData() async {
-    QuerySnapshot snapshot=await databaseReference
+    QuerySnapshot snapshot = await databaseReference
         .collection("nots")
         .getDocuments()
         .then((QuerySnapshot snapshot) {
       snapshot.documents.forEach((f) => v.add(new Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: InkWell(
-          child: Card(
-            elevation: 5.0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Center(
-                    child: Text(f.data['title'],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: textScaleFactor*20,
-                          fontFamily: 'PfDin',
-                          fontWeight: FontWeight.w800
-                      ),),
-                  ),
-
-                  Center(
-                      child: Text(f.data['description'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: textScaleFactor*15,
-                            fontFamily: 'PfDin',
-                            fontWeight: FontWeight.w500
-                        ),)),
-                  Center(
-                      child: InkWell(
-                        onTap: () {
-                          if(f.data['link']!=null)
-                          _launchURL(f.data['link']);
-                        },
-                        child: Text(f.data['link']==null?"":f.data['link'],
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              child: Card(
+                elevation: 5.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: Text(
+                          f.data['title'],
                           textAlign: TextAlign.center,
                           style: TextStyle(
-                            fontSize: textScaleFactor*15,
-                            fontFamily: 'PfDin',
-                            fontWeight: FontWeight.w500,
-                            color: Colors.blue,
-                          ),)
+                              fontSize: textScaleFactor * 20,
+                              fontFamily: 'PfDin',
+                              fontWeight: FontWeight.w800),
+                        ),
                       ),
+                      Center(
+                          child: Text(
+                        f.data['description'],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: textScaleFactor * 15,
+                            fontFamily: 'PfDin',
+                            fontWeight: FontWeight.w500),
+                      )),
+                      Center(
+                        child: InkWell(
+                            onTap: () {
+                              if (f.data['link'] != null)
+                                _launchURL(f.data['link']);
+                            },
+                            child: Text(
+                              f.data['link'] == null ? "" : f.data['link'],
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: textScaleFactor * 15,
+                                fontFamily: 'PfDin',
+                                fontWeight: FontWeight.w500,
+                                color: Colors.blue,
+                              ),
+                            )),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      )));
+          )));
       return snapshot;
     });
     snapshot.documents.forEach((f) => print(snapshot.documents.length));
     return snapshot;
   }
+
   _launchURL(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
