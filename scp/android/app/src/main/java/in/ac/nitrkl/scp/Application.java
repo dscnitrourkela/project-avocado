8f0ef1ff6bdd94389da86eb9766c2cc01beef0ad
Astha Nayak <nayakastha2911@gmail.com>
2021-11-11 00:52:56 +0530

    firebasemeassaging service android plug in fixed

--- scp/android/app/src/main/java/in/ac/nitrkl/scp/Application.java
+++ scp/android/app/src/main/java/in/ac/nitrkl/scp/Application.java
@@ -5,7 +5,6 @@ import io.flutter.plugin.common.PluginRegistry;
 import androidx.annotation.NonNull;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.android.FlutterActivity;
 
