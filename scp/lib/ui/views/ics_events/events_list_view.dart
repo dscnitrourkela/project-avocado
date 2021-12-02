19b289c8139a4355afb60cd320d35f51cb869080
abhibhaw <39991296+abhibhaw@users.noreply.github.com>
2021-12-03 03:56:43 +0530

    refactor: fixes mentor,mentee queries and url launcher as utils

--- scp/lib/ui/views/ics_events/events_list_view.dart
+++ scp/lib/ui/views/ics_events/events_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:scp/ui/dsc_social.dart';
+import 'package:scp/utils/urlLauncher.dart';
 
 class EventsListView extends StatelessWidget {
   const EventsListView({
